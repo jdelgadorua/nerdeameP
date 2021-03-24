a5f6c41b20d243cae8fff36104c4b68b6d8dd050
jrua <jdelgadorua90@gmail.com>
2021-03-24 03:31:39 -0400

    subir archivos

---
+++ NerdeameP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NerdeameP
+//
+//  Created by Jorgito on 3/9/21.
+//  Copyright © 2021 JodeRua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,9 @@
+ platform :ios, '12.0'
+
+target 'NerdeameP' do
+  use_frameworks!
+  
+  pod 'SVProgressHUD'
+  
+end
+
